280709423c38a651678af29a9dccc57574aa2200
Miguel Rodríguez <jazmindemar33@gmail.com>
2026-01-06 17:23:36 +0100

    Security updated

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(title = "Weavile Api", version = "1.0.0"),
+	servers = {
+		@Server(url = "http://localhost:9002")
+	},
+	tags = {
+		@Tag(name = "Item", description = "Pokemon item")
+	}
+)
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -144,7 +144,11 @@ public class SecurityConfig {
             httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
             http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/nonLoggedUsers/**","/pokemonData/**","/natureData/**","/itemData/allItems","/something_something/**").permitAll()
+                .requestMatchers(
+                "/nonLoggedUsers/**","/pokemonData/**","/natureData/**",
+                    "/itemData/allItems","/something_something/**",
+                    "/swagger-ui/**","/v3/api-docs/**","/swagger-ui.html"
+                ).permitAll()
                 .anyRequest().authenticated());
 
             /* Asegura que tu configuración de cors se aplique */
